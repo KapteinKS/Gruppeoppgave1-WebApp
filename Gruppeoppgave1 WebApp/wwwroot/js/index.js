64ab80bf6fb66fca0ebf6843765638b6384a5dc0
KapteinKS <sander.toien@outlook.com>
2021-09-21 14:28:39 +0200

    More dynamic stuff
    Picking route now bring up the datepicker. picking date now brings up departure time. picking eparture time now brings up the next page button

--- Gruppeoppgave1 WebApp/wwwroot/js/index.js
+++ Gruppeoppgave1 WebApp/wwwroot/js/index.js
@@ -5,32 +5,44 @@
 });
 
 function getRoutes(departures) {
-    let out = "<select id='routes' onchange='getDepartures()' class='routes_dropdown'>" + 
-        "<option selected>Velg rute</option>";
+    let out = "<option selected>Velg rute</option>";
+    /*"<select id='routes' onchange='getDepartures()' class='routes_dropdown'>" +
+        "<option selected>Velg rute</option>";*/
     
     for (let departure of departures) {            
         out += "<option value='" + departure.dep_location + "-" + departure.arr_location + "'>"
             + departure.dep_location + " - " + departure.arr_location + "</option>";
     }
-    out += "</select>";
-    $("#route_dropdown").html(out);
+    $("#routes").html(out);
+}
+
+function getDates() {
+    let out = "<input class=\"routes_dropdown\" type=\"date\" onchange=\"getDepartures()\"/>";
+    $("#date").html(out);
 }
 
 function getDepartures() {
     const route = $("#routes").val();
+    console.log(route);
     $.get("ticket/getDepartures", function (deps) {
-        let out = "<select class='routes_dropdown'>" + 
-            "<option selected>Velg avreisetid</option>";
+        let out = "<select class='routes_dropdown' onchange='nextWindow()'>" + 
+            "<option>Velg avreisetid</option>";
         for (const dep of deps) {
             let temp = dep.dep_location + "-" + dep.arr_location;
+            console.log(temp);
             if (temp === route) {
                 out += "<option>" + dep.dep_time + "</option >";
-                break;
             }
         }
             
-        out += "</select >";
+        out += "</select>";
 
         $("#times").html(out);
     });
+}
+
+function nextWindow() {
+    //<button onclick="window.location.href = 'purchase.html';" class="routes_dropdown">Neste</button>
+    let out = "<button onclick=\"window.location.href = \'purchase.html\';\" class=\"routes_dropdown\">Neste</button>";
+    $("#nextWindow").html(out);
 }
